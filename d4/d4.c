d410ab1129c3cbe84869a8d2e36c5ccd96641b99
Bavo Meyvis <bavomeyvis@protonmail.com>
2022-12-17 17:42:02 +0100

    Finished algorithm

--- d4/d4.c
+++ d4/d4.c
@@ -1,53 +1,54 @@
-# include<stdio.h>
+#include<stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <ctype.h>
-
-//91-93,6-92
-//85-97,18-63
-//15-99,16-98
-//35-49,34-50
-//4-7,6-93
-//57-86,57-58
-//1-82,83-90
-//21-88,20-89
-//83-84,1-83
-//8-82,7-83
 
 //First, make it work, then, make it pretty
 int main() {
     FILE* file = fopen("input.txt", "r"); /* should check the result */
 
     // Variables for puzzle 1
-    //int total_overlapping_pairs=0;
+    int full_overlaps=0;
+    int overlaps=0;
     int lineNr=0;    // line number
 
     char line[13] = {0};        // the line
-    char leftAss[2][3] = {0};   // left of the  ','
-    char rightAss[2][3] = {0};  // right of the ','
+    char leftElf[2][3] = {0};   // left of the  ','
+    char rightElf[2][3] = {0};  // right of the ','
 
     char buffer[13] = {0}; // buffer for lines read
     while(fgets(buffer, 13, file)) {
         memcpy(&line, &buffer[0], 13);
-        char * tok_ptr; // pointer for traversal
+        char * p = line; // pointer for traversal
+        int pos = 0;
+        while (*p) { // While there are more characters to process...
+            if ( isdigit(*p) && *p >= 0 ) {
+                // Found a number
+                long val = (int)strtol(p, &p, 10);
+                if (pos == 0)       memcpy(&leftElf[0], &val, sizeof(char));
+                else if (pos == 1)  memcpy(&leftElf[1], &val, sizeof(char));
+                else if (pos == 2)  memcpy(&rightElf[0], &val, sizeof(char));
+                else                memcpy(&rightElf[1], &val, sizeof(char));
+                pos++;
+            } else {
+                p++; // Otherwise, move on to the next character.
+            }
+        }
 
-        printf("\n\nL%d: %s \n", lineNr, line);
+        if ((*leftElf[0] <= *rightElf[0] && *leftElf[1] >= *rightElf[1]) || (*rightElf[0] <= *leftElf[0] && *rightElf[1] >= *leftElf[1])) {
+            full_overlaps += 1;
+        }
 
+        if (*leftElf[0] <= *rightElf[1] && *leftElf[1] >= *rightElf[0]) {
+            overlaps += 1;
+        }
 
-       //tok_ptr = strtok (line,"-");
-       //strcpy(&leftAss[0], tok_ptr);
-       //tok_ptr = strtok (NULL, ",");
-       //strcpy(&leftAss[1], tok_ptr);
-       //tok_ptr = strtok (NULL, "-");
-       //strcpy(&rightAss[0], tok_ptr);
-       //tok_ptr = strtok (NULL, "-");
-       //strcpy(&rightAss[1], tok_ptr);
-
-        printf("%s - %s , %s - %s\n", leftAss[0], leftAss[1], rightAss[0], rightAss[1]);
+        // Prepare for next line
         memset(line, 0, 13);
         lineNr++;
     }
+    printf("Total full overlaps: %d\nTotal overlaps:\t%d\n", full_overlaps, overlaps);
     fclose(file);
     return 0;
 }
